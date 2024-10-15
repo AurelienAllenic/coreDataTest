8da41d8427e6b5ebfadb8e22b2d98357c107d8ce
ALLENIC Aurélien <aa244355@dgkyrhejjwf2.edu.atlas.local>
2024-10-15 13:54:32 +0200

    (feat) first commit after creation of project

---
+++ coreDataTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  coreDataTest
+//
+//  Created by ALLENIC Aurélien on 15/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ coreDataTest/coreDataTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  coreDataTestApp.swift
+//  coreDataTest
+//
+//  Created by ALLENIC Aurélien on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct coreDataTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
